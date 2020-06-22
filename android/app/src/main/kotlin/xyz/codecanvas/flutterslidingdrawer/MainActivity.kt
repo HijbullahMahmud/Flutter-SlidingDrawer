49b44a0880d90f8d390dcbcda30b6af6fe32918c
Hijbullah Al Mahmud <hijbullahmahmud14@gmail.com>
2020-06-22 22:42:33 +0600

    initial commit

---
+++ android/app/src/main/kotlin/xyz/codecanvas/flutterslidingdrawer/MainActivity.kt
@@ -0,0 +1,6 @@
+package xyz.codecanvas.flutterslidingdrawer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutterslidingdrawer/menu_dashboard_layout.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: MenuDashboardPage(),
+  ));
+}
+
+
